d19269714ca9cddbeaeac2c724bede7dff5bf7c0
Yura Kocyuba <yurakocyuba@yandex.ru>
2019-11-19 17:45:57 +0200

    added scroll api to get_actions endpoint

--- src/main/java/eosio/history/rest/controller/GetActions.java
+++ src/main/java/eosio/history/rest/controller/GetActions.java
@@ -8,15 +8,17 @@ import eosio.history.rest.config.Properties;
 import org.elasticsearch.ElasticsearchException;
 import org.elasticsearch.ElasticsearchStatusException;
 import org.elasticsearch.ElasticsearchTimeoutException;
-import org.elasticsearch.action.search.SearchRequest;
-import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.search.*;
 import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.Scroll;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHits;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.search.sort.FieldSortBuilder;
 import org.elasticsearch.search.sort.SortOrder;
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -89,24 +91,22 @@ public class GetActions {
                 ));
         SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
 
-        if (pos < 0) {
-            if (offset < 0){
-                from = Math.abs(pos)-1;
-                size = Math.abs(offset);
-                sortOrder = SortOrder.DESC;
-            }
-        }else if (pos == 0 & offset ==0){
+
+        if (pos == 0 & offset ==0){
             from = 0;
             size = 1000;
-        } else {
-            if (offset < 0) {
-                from = 0;
-                size = pos + 1;
-            } else {
-                from = pos;
-                size = offset + 1;
-            }
+        }else  if (pos < 0) {
+                from = Math.abs(pos)-1;
+                size = Math.abs(offset);
+            sortOrder = SortOrder.DESC;
+        }else if ( pos  > 0 ){
+                from = pos-1;
+                size = Math.abs(offset);
+        }else if ( pos  == 0 ){
+            from = pos;
+            size = Math.abs(offset);
         }
+
         if (actions.getAfter() != null){
             gte = actions.getAfter();
         }
@@ -119,44 +119,98 @@ public class GetActions {
             gte="now-"+actions.getLast();
             sortOrder = SortOrder.DESC;
         }
-
-
         ((BoolQueryBuilder) queryBuilder).filter().add(QueryBuilders.rangeQuery("block_timestamp").gte(gte).lte(lte));
 
         searchSourceBuilder.sort("receipt.global_sequence", sortOrder);
         searchSourceBuilder.from(from);
         searchSourceBuilder.size(size);
         searchSourceBuilder.query(queryBuilder);
+
+        if (size+from > 10000){
+            response.put("actions", scrollRequest(searchSourceBuilder, from, size));
+        } else {
+            try {
+                searchResponse = elasticSearchClient.getElasticsearchClient().search(searchRequest, RequestOptions.DEFAULT);
+            } catch (ElasticsearchTimeoutException e) {
+                return new ResponseEntity<>(e.getDetailedMessage(), httpHeaders, HttpStatus.REQUEST_TIMEOUT);
+            } catch (ElasticsearchException e) {
+                return new ResponseEntity<>(e.getDetailedMessage(), httpHeaders, HttpStatus.BAD_REQUEST);
+            }
+            SearchHits hits = searchResponse.getHits();
+            if (hits.getTotalHits().value == 0) {
+                logger.info("Reuqest: " + actions.toString() + " response: " + HttpStatus.NOT_FOUND);
+                return new ResponseEntity<>("", httpHeaders, HttpStatus.NOT_FOUND);
+            }
+            for (SearchHit hit : hits) {
+                JSONObject jsonObjectActions = new JSONObject(hit.getSourceAsString());
+                String data = jsonObjectActions.getJSONObject("act").getString("data");
+                JSONObject jsonObjectData = new JSONObject(data);
+                jsonObjectActions.getJSONObject("act").put("data", jsonObjectData);
+                jsonObjectActions.remove("trx");
+                jsons.put(jsonObjectActions);
+            }
+            response.put("query_time", searchResponse.getTook().toString());
+            response.put("actions", jsons);
+            logger.info("Reuqest: "+actions.toString()+" response: "+HttpStatus.OK +" query_time: "+searchResponse.getTook().toString()+" actions: "+jsons.length());
+        }
+        return new ResponseEntity<>(response.toString(), httpHeaders, HttpStatus.OK);
+    }
+
+    private JSONArray scrollRequest(SearchSourceBuilder searchSourceBuilder, int from, int size) throws IOException {
+        int scrollPage = 1;
+        int pagesSize = 10000;
+        final Scroll scroll = new Scroll(TimeValue.timeValueMinutes(1L));
+        searchSourceBuilder.size(pagesSize).from(0);
+        SearchRequest searchRequest = new SearchRequest(get_actions_index);
+        searchRequest.scroll(scroll);
         searchRequest.source(searchSourceBuilder);
-        try {
-            searchResponse = elasticSearchClient.getElasticsearchClient().search(searchRequest, RequestOptions.DEFAULT);
-        } catch (ElasticsearchTimeoutException e){
-            return new ResponseEntity<>(e.getDetailedMessage(), httpHeaders, HttpStatus.REQUEST_TIMEOUT);
+        SearchResponse searchResponse = elasticSearchClient.getElasticsearchClient().search(searchRequest, RequestOptions.DEFAULT);
+        String scrollId = searchResponse.getScrollId();
+        SearchHit[] searchHits = searchResponse.getHits().getHits();
+//        if (pagesSize*scrollPage - from > 0 ){
+//           List<SearchHit> subArray = Arrays.asList(searchHits).subList(Math.abs(scrollPage-from-scrollSize), Math.abs(scrollPage-from-scrollSize)+size);
+//        }
+        while (searchHits != null && searchHits.length > 0) {
+            List<SearchHit> searchHitsList;
+            ++scrollPage;
+            SearchScrollRequest scrollRequest = new SearchScrollRequest(scrollId);
+            scrollRequest.scroll(scroll);
+            searchResponse = elasticSearchClient.getElasticsearchClient().scroll(scrollRequest, RequestOptions.DEFAULT);
+            scrollId = searchResponse.getScrollId();
+            searchHits = searchResponse.getHits().getHits();
+            if (pagesSize*scrollPage-from > 0){
+                try {
+                    searchHitsList = Arrays.asList(searchHits).subList(Math.abs(pagesSize*(scrollPage-1)-from), Math.abs(pagesSize*(scrollPage-1)-from)+size);
+                    clearScrollRequest(scrollId);
+                    return hitsProcessing(searchHitsList);
+                } catch (IndexOutOfBoundsException exc){
+                    searchHitsList = Arrays.asList(searchHits).subList(Math.abs(pagesSize*(scrollPage-1)-from), searchHits.length-1);
+                    clearScrollRequest(scrollId);
+                    return hitsProcessing(searchHitsList);
+                }
+            }
         }
-        catch (ElasticsearchException e){
-            return new ResponseEntity<>(e.getDetailedMessage(), httpHeaders, HttpStatus.BAD_REQUEST);
-        }
-        SearchHits hits = searchResponse.getHits();
-
-        if (hits.getTotalHits().value == 0){
-            logger.info("Reuqest: "+actions.toString()+" response: "+HttpStatus.NOT_FOUND);
-            return new ResponseEntity<>("", httpHeaders, HttpStatus.NOT_FOUND);
-        }
-
-        searchResponse.getTook();
-
+        clearScrollRequest(scrollId);
+        return new JSONArray();
+    }
+    private JSONArray hitsProcessing(List<SearchHit> hits){
+        JSONArray result = new JSONArray();
         for (SearchHit hit : hits) {
             JSONObject jsonObjectActions = new JSONObject(hit.getSourceAsString());
             String data = jsonObjectActions.getJSONObject("act").getString("data");
             JSONObject jsonObjectData = new JSONObject(data);
             jsonObjectActions.getJSONObject("act").put("data", jsonObjectData);
             jsonObjectActions.remove("trx");
-            jsons.put(jsonObjectActions);
+            result.put(jsonObjectActions);
         }
-
-        response.put("query_time", searchResponse.getTook().toString());
-        response.put("actions", jsons);
-        logger.info("Reuqest: "+actions.toString()+" response: "+HttpStatus.OK +" query_time: "+searchResponse.getTook().toString()+" actions: "+jsons.length());
-        return new ResponseEntity<>(response.toString(), httpHeaders, HttpStatus.OK);
+        return result;
     }
+
+    private Boolean clearScrollRequest(String scorllId) throws IOException {
+        ClearScrollRequest clearScrollRequest = new ClearScrollRequest();
+        clearScrollRequest.addScrollId(scorllId);
+        ClearScrollResponse clearScrollResponse = elasticSearchClient.getElasticsearchClient().clearScroll(clearScrollRequest, RequestOptions.DEFAULT);
+        return clearScrollResponse.isSucceeded();
+    }
+
 }
