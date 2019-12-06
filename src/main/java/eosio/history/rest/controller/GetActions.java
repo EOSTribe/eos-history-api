8d21ebfc0e12ec908a10d81ae355a17756b4ea66
Yura Kocyuba <yurakocyuba@yandex.ru>
2019-12-06 17:06:22 +0200

    fixed JSON exeception

--- src/main/java/eosio/history/rest/controller/GetActions.java
+++ src/main/java/eosio/history/rest/controller/GetActions.java
@@ -32,6 +32,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.sql.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -69,9 +70,7 @@ public class GetActions {
         String lte = "now";
         String gte = "0";
         SortOrder sortOrder = SortOrder.ASC;
-        JSONArray jsons = new JSONArray();
-        JSONObject response = new JSONObject();
-        SearchResponse searchResponse = null;
+        JSONObject response;
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.setAccessControlAllowOrigin(accessControlAllowOrigin);
         httpHeaders.setAccessControlAllowHeaders(accessControlAllowHeaders);
@@ -79,7 +78,6 @@ public class GetActions {
         String account_name = actions.getAccount_name();
         int pos = actions.getPos();
         int offset = actions.getOffset();
-        SearchRequest searchRequest = new SearchRequest(get_actions_index);
 
         QueryBuilder queryBuilder =
                 new BoolQueryBuilder().
@@ -128,44 +126,18 @@ public class GetActions {
         searchSourceBuilder.query(queryBuilder);
 
         if (size+from > 10000){
-            response.put("actions", scrollRequest(searchSourceBuilder, from, size));
+            response = scrollRequest(searchSourceBuilder, from, size, actions);
         } else {
-            try {
-                searchResponse = elasticSearchClient.getElasticsearchClient().search(searchRequest, RequestOptions.DEFAULT);
-            } catch (ElasticsearchTimeoutException e) {
-                return new ResponseEntity<>(e.getDetailedMessage(), httpHeaders, HttpStatus.REQUEST_TIMEOUT);
-            } catch (ElasticsearchException e) {
-                return new ResponseEntity<>(e.getDetailedMessage(), httpHeaders, HttpStatus.BAD_REQUEST);
-            }
-            SearchHits hits = searchResponse.getHits();
-            if (hits.getTotalHits().value == 0) {
-                logger.info("Reuqest: " + actions.toString() + " response: " + HttpStatus.NOT_FOUND);
-                return new ResponseEntity<>("", httpHeaders, HttpStatus.NOT_FOUND);
-            }
-            for (SearchHit hit : hits) {
-                JSONObject jsonObjectData;
-                JSONObject jsonObjectActions = new JSONObject(hit.getSourceAsString());
-                String data = jsonObjectActions.getJSONObject("act").getString("data");
-                try {
-                    jsonObjectData = new JSONObject(data);
-                    jsonObjectActions.getJSONObject("act").put("data", jsonObjectData);
-                }catch (JSONException jse){
-                    logger.error(jse.getMessage());
-                }
-
-                jsonObjectActions.remove("trx");
-                jsons.put(jsonObjectActions);
-            }
-            response.put("query_time", searchResponse.getTook().toString());
-            response.put("actions", jsons);
-            logger.info("Reuqest: "+actions.toString()+" response: "+HttpStatus.OK +" query_time: "+searchResponse.getTook().toString()+" actions: "+jsons.length());
+            response = regularReuqest(searchSourceBuilder, actions);
         }
         return new ResponseEntity<>(response.toString(), httpHeaders, HttpStatus.OK);
     }
 
-    private JSONArray scrollRequest(SearchSourceBuilder searchSourceBuilder, int from, int size) throws IOException {
+    private JSONObject scrollRequest(SearchSourceBuilder searchSourceBuilder, int from, int size, Actions actions) throws IOException, ElasticsearchException {
+        JSONObject result = new JSONObject();
         int scrollPage = 1;
         int pagesSize = 10000;
+        long requestDurationSec;
         final Scroll scroll = new Scroll(TimeValue.timeValueMinutes(1L));
         searchSourceBuilder.size(pagesSize).from(0);
         SearchRequest searchRequest = new SearchRequest(get_actions_index);
@@ -174,6 +146,8 @@ public class GetActions {
         SearchResponse searchResponse = elasticSearchClient.getElasticsearchClient().search(searchRequest, RequestOptions.DEFAULT);
         String scrollId = searchResponse.getScrollId();
         SearchHit[] searchHits = searchResponse.getHits().getHits();
+        requestDurationSec = searchResponse.getTook().millis();
+
 //        if (pagesSize*scrollPage - from > 0 ){
 //           List<SearchHit> subArray = Arrays.asList(searchHits).subList(Math.abs(scrollPage-from-scrollSize), Math.abs(scrollPage-from-scrollSize)+size);
 //        }
@@ -185,33 +159,57 @@ public class GetActions {
             searchResponse = elasticSearchClient.getElasticsearchClient().scroll(scrollRequest, RequestOptions.DEFAULT);
             scrollId = searchResponse.getScrollId();
             searchHits = searchResponse.getHits().getHits();
+            requestDurationSec = requestDurationSec+searchResponse.getTook().millis();
+
             if (pagesSize*scrollPage-from > 0){
                 try {
                     searchHitsList = Arrays.asList(searchHits).subList(Math.abs(pagesSize*(scrollPage-1)-from), Math.abs(pagesSize*(scrollPage-1)-from)+size);
                     clearScrollRequest(scrollId);
-                    logger.info(" query_time: "+searchResponse.getTook().toString()+" actions: "+searchHitsList.size());
-
-                    return hitsProcessing(searchHitsList);
+                    logger.info(" query_time: "+requestDurationSec+"ms " +" actions: "+searchHitsList.size());
+                    result.put("query_time", requestDurationSec+"ms");
+                    result.put("actions", hitsProcessing(searchHitsList));
+                    return result;
                 } catch (IndexOutOfBoundsException exc){
                     searchHitsList = Arrays.asList(searchHits).subList(Math.abs(pagesSize*(scrollPage-1)-from), searchHits.length-1);
                     clearScrollRequest(scrollId);
-                    return hitsProcessing(searchHitsList);
+                    result.put("query_time", requestDurationSec+"ms");
+                    result.put("actions", hitsProcessing(searchHitsList));
+                    logger.info("Reuqest: "+actions.toString()+" response: "+HttpStatus.OK +" query_time: "+searchResponse.getTook().millis()+"ms "+" actions: "+searchHitsList.size());
+                    return result;
                 }
             }
         }
         clearScrollRequest(scrollId);
-        return new JSONArray();
+        return result;
     }
     private JSONArray hitsProcessing(List<SearchHit> hits){
         JSONArray result = new JSONArray();
         for (SearchHit hit : hits) {
             JSONObject jsonObjectActions = new JSONObject(hit.getSourceAsString());
             String data = jsonObjectActions.getJSONObject("act").getString("data");
-            JSONObject jsonObjectData = new JSONObject(data);
-            jsonObjectActions.getJSONObject("act").put("data", jsonObjectData);
+            try {
+                JSONObject jsonObjectData = new JSONObject(data);
+                jsonObjectActions.getJSONObject("act").put("data", jsonObjectData);
+            }catch (JSONException jse){
+                logger.error(jse.getMessage());
+            }
             jsonObjectActions.remove("trx");
             result.put(jsonObjectActions);
         }
+        return result;
+    }
+
+    private JSONObject regularReuqest(SearchSourceBuilder searchSourceBuilder, Actions actions) throws IOException, ElasticsearchException {
+        JSONObject result = new JSONObject();
+        JSONArray actionsArray = new JSONArray();
+        SearchRequest searchRequest = new SearchRequest(get_actions_index);
+        searchRequest.source(searchSourceBuilder);
+        SearchResponse searchResponse = elasticSearchClient.getElasticsearchClient().search(searchRequest, RequestOptions.DEFAULT);
+
+        List<SearchHit> searchHits = Arrays.asList(searchResponse.getHits().getHits());
+        result.put("query_time", searchResponse.getTook().getMillis()+"ms");
+        result.put("actions", hitsProcessing(searchHits));
+        logger.info("Reuqest: "+actions.toString()+" response: "+HttpStatus.OK +" query_time: "+searchResponse.getTook().millis()+"ms "+" actions: "+actionsArray.length());
         return result;
     }
 
