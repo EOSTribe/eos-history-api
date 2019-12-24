37a59dc8dc5190c60d36d1d3a825cf783ab21a49
Yura Kocyuba <yurakocyuba@yandex.ru>
2019-12-24 16:40:27 +0200

    getControlledAccounts

---
+++ src/main/java/eosio/history/rest/Account.java
@@ -0,0 +1,13 @@
+package eosio.history.rest;
+
+public class Account {
+    private String controlling_account;
+
+    public String getControlling_account() {
+        return controlling_account;
+    }
+
+    public void setControlling_account(String controlling_account) {
+        this.controlling_account = controlling_account;
+    }
+}
--- src/main/java/eosio/history/rest/controller/GetControlledAcctounts.java
+++ src/main/java/eosio/history/rest/controller/GetControlledAcctounts.java
@@ -1,6 +1,8 @@
 package eosio.history.rest.controller;
 
 
+import com.google.gson.JsonObject;
+import eosio.history.rest.Account;
 import eosio.history.rest.ElasticSearchClient;
 import eosio.history.rest.Key;
 import eosio.history.rest.KeyConvertor;
@@ -10,8 +12,10 @@ import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.MultiMatchQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.search.MultiMatchQuery;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHits;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
@@ -20,30 +24,22 @@ import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 @RestController
-@RequestMapping("/v1/history/get_key_accounts")
-public class GetKeyAccounts {
-    private static final transient Logger logger = LoggerFactory.getLogger(GetKeyAccounts.class);
+@RequestMapping("/v1/history/get_controlled_accounts")
+public class GetControlledAcctounts {
+    private static final transient Logger logger = LoggerFactory.getLogger(GetControlledAcctounts.class);
 
-    private KeyConvertor keyConvertor;
     private ElasticSearchClient elasticSearchClient;
     private String get_key_accounts_index;
 
-    @Autowired
-    public void setKeyConvertor(KeyConvertor keyConvertor){
-        this.keyConvertor = keyConvertor;
-    }
     @Autowired
     public void setProperties(Properties properties){
         this.get_key_accounts_index = properties.getNewAccountIndex();
@@ -55,16 +51,10 @@ public class GetKeyAccounts {
 
     @CrossOrigin
     @RequestMapping(method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
-    ResponseEntity<?> get_actions(@RequestBody Key key) throws IOException, EOSFormatterError {
-        Set accounts = new HashSet();
+    ResponseEntity<?> get_actions(@RequestBody Account account) throws IOException, EOSFormatterError {
         JSONObject response = new JSONObject();
-        String publicKeyK1;
-        if (key.getKey().substring(0,3).equals("EOS")){
-            publicKeyK1 = keyConvertor.fromLegacyToK1PublicKey(key.getKey());
-        }else {
-            publicKeyK1 = key.getKey();
-        }
-        QueryBuilder queryBuilder = new BoolQueryBuilder().filter(QueryBuilders.boolQuery().minimumShouldMatch(1).should(QueryBuilders.matchQuery("act.data",publicKeyK1)));
+        QueryBuilder queryBuilder = new BoolQueryBuilder().filter(
+                new MultiMatchQueryBuilder(account.getControlling_account()));
         SearchRequest searchRequest = new SearchRequest(get_key_accounts_index);
         SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
         searchSourceBuilder.query(queryBuilder);
@@ -73,8 +63,9 @@ public class GetKeyAccounts {
         SearchResponse searchResponse = elasticSearchClient.getElasticsearchClient().search(searchRequest, RequestOptions.DEFAULT);
         SearchHits searchHits = searchResponse.getHits();
         response.put("query_time", searchResponse.getTook().getMillis()+"ms");
+
         if (searchHits.getTotalHits().value == 0){
-            logger.info("Reuqest: "+key.getKey()+" response: "+HttpStatus.NOT_FOUND +" query_time: "+searchResponse.getTook().millis()+"ms ");
+            logger.info("Reuqest: "+account.getControlling_account()+" response: "+HttpStatus.NOT_FOUND +" query_time: "+searchResponse.getTook().millis()+"ms ");
             return new ResponseEntity<>(response.toString(), HttpStatus.NOT_FOUND);
         }
         for (SearchHit hit : searchHits) {
@@ -88,20 +79,20 @@ public class GetKeyAccounts {
             }
 
             try {
-                accounts.add(jsonObjectData.getString("name"));
+                jsonObjectData.getString("name");
 
             }catch (JSONException jse){
                 logger.warn(jse.getMessage());
             }
             try {
 
-                accounts.add(jsonObjectData.getString("account"));
+//                accounts.add(jsonObjectData.getString("account"));
             }catch (JSONException jse){
                 logger.warn(jse.getMessage());
             }
         }
-        response.put("accounts",accounts);
-        logger.info("Reuqest: "+key.getKey()+" response: "+HttpStatus.OK +" query_time: "+searchResponse.getTook().millis()+"ms ");
+//        response.put("accounts",accounts);
+//        logger.info("Reuqest: "+key.getKey()+" response: "+HttpStatus.OK +" query_time: "+searchResponse.getTook().millis()+"ms ");
         return new ResponseEntity<>(response.toString(), HttpStatus.OK);
     }
 }
